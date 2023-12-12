5ec4bf6304430f7986ea96b1cb3e2aaf732d4cc2
Ivan Vartanyan <12048256+0xevn@users.noreply.github.com>
2023-12-13 02:46:07 +0300

    Fix: DB version bump

--- src/shared/lib/db.js
+++ src/shared/lib/db.js
@@ -2,6 +2,6 @@ import Dexie from "dexie";
 
 export const db = new Dexie("Seal");
 
-db.version(1).stores({
+db.version(2).stores({
     files: "id, cid, pid, name, size, mimeType, status, encryption, created, updated, protocol, storageProvider, txHash, publicKey, url"
 });
